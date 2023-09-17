4b28ef3e6abe4bf96a298ca8bf09afaad7a080d4
Informática Primordial <principalcontat@gmail.com>
2023-09-17 12:49:37 -0300

    random_

---
+++ programas_/random.py
@@ -0,0 +1,10 @@
+#sorteio de 1 aluno entre 4
+
+import random  #pode ser também from random import choice
+aluno1 = str(input('Primeiro aluno: '))
+aluno2 = str(input('Segundo aluno: '))
+aluno3 = str(input('Terceiro aluno: '))
+aluno4 = str(input('Quarto aluno'))
+lista = [aluno1, aluno2, aluno3, aluno4]
+escolhido = random.choice(lista)
+print('O aluno escolhido foi: {}'.format(escolhido))
